d312463e843bc535f78f2c2ad7c2ba6a997808aa
Daniel Bravo <dani.bravos@duocuc.cl>
2025-07-02 20:40:21 -0400

    last prueba

--- microservice-product/src/main/java/com/microservice/product/MicroserviceproductApplication.java
+++ microservice-product/src/main/java/com/microservice/product/MicroserviceproductApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceproductApplication {
-
+//prueba
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceproductApplication.class, args);
 	}
